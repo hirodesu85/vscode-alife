b06a19dcb13e86489400f6845811d7bf34936038
OGATA Hiromu <hiromukkun@gmail.com>
2025-03-22 16:19:01 +0900

    add:画像で動くようにする

--- src/WebViewProvider.ts
+++ src/WebViewProvider.ts
@@ -8,13 +8,18 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
       enableScripts: true,
     };
 
+    // 画像のURIを取得
+    const boidImageUri = webviewView.webview.asWebviewUri(
+      vscode.Uri.joinPath(this.extensionUri, "resources", "images", "boid.png")
+    );
+
     webviewView.webview.html = `
       <!DOCTYPE html>
       <html lang="en">
         <head>
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Boid Simulation with Pixel Art</title>
+          <title>Boid Simulation</title>
           <style>
             body {
               margin: 0;
@@ -28,69 +33,7 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
         <body>
           <canvas id="canvas"></canvas>
           <script>
-            const BOID_SIZE = 1;
             const MAX_SPEED = 2;
-
-            const pixelArt = [
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 4, 4, 4, 4, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 4, 2, 5, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 4, 4, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 3, 2, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 4, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2, 5, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 1, 4, 4, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 2, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 1, 4, 3, 6, 6, 6, 6, 6, 6, 6, 6, 3, 4, 1, 1, 1, 6, 6, 6, 5, 5, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 1, 1, 1, 4, 3, 6, 6, 6, 6, 6, 6, 3, 4, 1, 1, 1, 1, 1, 6, 6, 6, 6, 4, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 1, 2, 2, 2, 1, 4, 6, 6, 6, 6, 6, 6, 4, 1, 1, 2, 2, 2, 1, 1, 6, 6, 6, 6, 4, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 2, 2, 2, 1, 1, 1, 6, 6, 6, 6, 6, 6, 1, 1, 2, 2, 2, 2, 2, 1, 1, 6, 6, 6, 4, 4, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 1, 1, 2, 2, 1, 2, 1, 1, 6, 6, 6, 6, 6, 6, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 6, 6, 4, 4, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1, 2, 1, 7, 1, 6, 6, 5, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1, 1, 7, 4, 1, 6, 6, 5, 5, 5, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 7, 6, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1, 7, 5, 4, 1, 4, 6, 6, 5, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 1, 4, 1, 1, 1, 7, 7, 6, 6, 3, 3, 6, 6, 6, 1, 1, 7, 3, 7, 7, 5, 4, 11, 1, 4, 6, 6, 6, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 1, 8, 7, 4, 7, 3, 5, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 3, 3, 3, 5, 2, 4, 1, 4, 6, 6, 6, 6, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 1, 10, 2, 1, 11, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 8, 8, 8, 6, 6, 6, 6, 5, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 1, 10, 9, 9, 1, 1, 1, 2, 2, 1, 6, 11, 11, 6, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 0, 8, 6, 6, 6, 5, 8, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 1, 8, 10, 10, 8, 2, 2, 2, 2, 2, 1, 6, 6, 1, 2, 2, 2, 2, 2, 2, 2, 8, 8, 8, 0, 0, 8, 6, 6, 5, 7, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-              [0, 1, 8, 8, 8, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 8, 8, 8, 8, 8, 8, 5, 5, 7, 10, 1, 0, 1, 4, 4, 4, 1, 0, 0, 1, 4, 4, 1, 0, 0],
-              [0, 1, 10, 8, 8, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 8, 8, 8, 8, 8, 5, 5, 7, 10, 10, 0, 1, 4, 4, 2, 3, 3, 1, 1, 4, 2, 3, 4, 1, 0],
-              [0, 0, 1, 10, 8, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 8, 8, 8, 8, 8, 7, 7, 10, 1, 0, 1, 4, 6, 2, 2, 5, 3, 5, 2, 2, 6, 4, 1, 0],
-              [0, 0, 0, 1, 8, 9, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 8, 8, 8, 7, 10, 1, 0, 0, 5, 5, 6, 6, 2, 2, 2, 2, 2, 6, 6, 3, 1, 0],
-              [0, 0, 0, 0, 1, 1, 9, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 7, 7, 8, 1, 0, 0, 0, 0, 5, 6, 6, 6, 6, 2, 6, 6, 6, 6, 5, 1, 0],
-              [0, 0, 0, 0, 0, 0, 1, 10, 9, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 9, 8, 1, 1, 0, 0, 0, 0, 1, 5, 3, 6, 6, 6, 6, 6, 4, 5, 0, 1, 0],
-              [0, 0, 0, 0, 0, 0, 0, 1, 9, 9, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 9, 10, 1, 1, 3, 3, 1, 0, 0, 0, 0, 1, 3, 3, 6, 6, 6, 4, 5, 5, 8, 1, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 8, 9, 2, 2, 2, 2, 2, 2, 2, 2, 9, 8, 1, 1, 1, 1, 3, 3, 3, 5, 5, 1, 0, 0, 0, 0, 10, 3, 6, 6, 5, 5, 0, 10, 1, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 8, 9, 9, 9, 9, 9, 9, 9, 9, 8, 6, 5, 5, 5, 1, 3, 3, 5, 6, 6, 6, 1, 0, 1, 1, 8, 6, 6, 5, 8, 8, 10, 1, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 9, 9, 9, 9, 9, 9, 9, 9, 6, 6, 6, 6, 6, 4, 1, 3, 6, 6, 6, 6, 6, 1, 6, 6, 6, 6, 5, 5, 1, 1, 1, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 1, 1, 5, 1, 2, 2, 2, 2, 2, 2, 2, 2, 9, 4, 6, 6, 6, 6, 6, 4, 1, 3, 3, 6, 6, 6, 6, 6, 6, 6, 5, 8, 9, 1, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 1, 4, 4, 5, 1, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 4, 6, 6, 4, 4, 5, 1, 3, 3, 6, 6, 6, 6, 6, 6, 6, 8, 9, 9, 1, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 1, 4, 4, 5, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 9, 1, 4, 4, 5, 5, 6, 1, 3, 3, 3, 6, 6, 6, 6, 6, 8, 8, 9, 9, 1, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 1, 6, 5, 5, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 1, 1, 1, 1, 1, 8, 3, 3, 3, 6, 6, 6, 5, 5, 8, 8, 9, 9, 1, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 9, 9, 9, 8, 8, 8, 8, 8, 3, 3, 5, 8, 8, 8, 9, 9, 1, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 9, 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 9, 9, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 9, 9, 9, 8, 8, 8, 9, 9, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 9, 9, 9, 9, 9, 9, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 9, 9, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ];
-
-            const backgroundArt = [
-              [0, 1, 1, 0, 1, 1, 1, 0],
-              [1, 0, 0, 1, 0, 0, 0, 1],
-              [1, 0, 1, 1, 1, 0, 0, 1],
-              [0, 1, 0, 0, 0, 1, 1, 0],
-              [1, 0, 1, 0, 1, 0, 0, 1],
-              [0, 1, 1, 1, 1, 1, 1, 0],
-              [1, 0, 1, 0, 0, 1, 0, 1],
-              [0, 1, 0, 1, 1, 0, 1, 0],
-            ];
 
             class Boids {
               constructor() {
@@ -100,14 +43,20 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                 this.offscreenCtx = this.offscreenCanvas.getContext("2d");
                 this.boids = [];
                 this.view = { width: 0, height: 0 };
+                this.boidImage = new Image();
+                this.boidImage.src = "${boidImageUri}";
               }
+
               init() {
-                this.bindEvents();
-                this.updateView();
-                this.drawBackground();
-                this.appendBoids(3);
-                requestAnimationFrame(this.simulate.bind(this));
+                this.boidImage.onload = () => {
+                  this.bindEvents();
+                  this.updateView();
+                  this.drawBackground();
+                  this.appendBoids(3);
+                  requestAnimationFrame(this.simulate.bind(this));
+                };
               }
+
               bindEvents() {
                 addEventListener("click", (e) => {
                   this.appendBoids(1, e.pageX, e.pageY);
@@ -117,6 +66,7 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                   this.drawBackground();
                 });
               }
+
               updateView() {
                 this.view = {
                   width: window.innerWidth,
@@ -127,29 +77,19 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                 this.offscreenCanvas.width = this.view.width;
                 this.offscreenCanvas.height = this.view.height;
               }
+
               simulate() {
                 this.ctx.drawImage(this.offscreenCanvas, 0, 0);
                 this.drawBoids();
                 this.moveBoids();
                 requestAnimationFrame(this.simulate.bind(this));
               }
+
               drawBackground() {
-                for (let y = 0; y < this.offscreenCanvas.height / BOID_SIZE; y++) {
-                  for (let x = 0; x < this.offscreenCanvas.width / BOID_SIZE; x++) {
-                    let color =
-                      backgroundArt[y % backgroundArt.length][
-                        x % backgroundArt[0].length
-                      ];
-                    this.offscreenCtx.fillStyle = color === 1 ? "#A0DFF7" : "#B5E8FF";
-                    this.offscreenCtx.fillRect(
-                      x * BOID_SIZE,
-                      y * BOID_SIZE,
-                      BOID_SIZE,
-                      BOID_SIZE
-                    );
-                  }
-                }
+                this.offscreenCtx.fillStyle = "#B5E8FF";
+                this.offscreenCtx.fillRect(0, 0, this.view.width, this.view.height);
               }
+
               appendBoids(length, x, y) {
                 if (this.boids.length >= 10) {
                   return;
@@ -164,68 +104,24 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                   });
                 }
               }
+
               drawBoids() {
                 for (let i = 0, len = this.boids.length; i < len; i++) {
-                  this.drawPixelArt(this.boids[i].x, this.boids[i].y, i);
-                }
-              }
-              drawPixelArt(x, y, index) {
-                for (let py = 0; py < pixelArt.length; py++) {
-                  for (let px = 0; px < pixelArt[py].length; px++) {
-                    let color;
-                    if (this.boids[index].vx > 0) {
-                      color = pixelArt[py][pixelArt[py].length - 1 - px];
-                    } else {
-                      color = pixelArt[py][px];
-                    }
-                    if (color !== 0) {
-                      switch (color) {
-                        case 1:
-                          this.ctx.fillStyle = "#000000"; // black
-                          break;
-                        case 2:
-                          this.ctx.fillStyle = "#FEFEF6"; // white
-                          break;
-                        case 3:
-                          this.ctx.fillStyle = "#193F91"; // blue1
-                          break;
-                        case 4:
-                          this.ctx.fillStyle = "#5D9CE0"; // blue2
-                          break;
-                        case 5:
-                          this.ctx.fillStyle = "#0A58AE"; // blue3
-                          break;
-                        case 6:
-                          this.ctx.fillStyle = "#3E5495"; // blue4
-                          break;
-                        case 7:
-                          this.ctx.fillStyle = "#182651"; // blue5
-                          break;
-                        case 8:
-                          this.ctx.fillStyle = "#F788B2"; // pink1
-                          break;
-                        case 9:
-                          this.ctx.fillStyle = "#F2B5CC"; // pink2
-                          break;
-                        case 10:
-                          this.ctx.fillStyle = "#DD6795"; // pink3
-                          break;
-                        case 11:
-                          this.ctx.fillStyle = "#B69DC9"; // purple
-                          break;
-                        default:
-                          this.ctx.fillStyle = "#000000"; // black
-                      }
-                      this.ctx.fillRect(
-                        x + px * BOID_SIZE,
-                        y + py * BOID_SIZE,
-                        BOID_SIZE,
-                        BOID_SIZE
-                      );
-                    }
+                  const boid = this.boids[i];
+                  this.ctx.save();
+                  this.ctx.translate(boid.x, boid.y);
+                  if (boid.vx > 0) {
+                    this.ctx.scale(-1, 1);
                   }
+                  this.ctx.drawImage(
+                    this.boidImage,
+                    -this.boidImage.width / 2,
+                    -this.boidImage.height / 2
+                  );
+                  this.ctx.restore();
                 }
               }
+
               moveBoids() {
                 for (let i = 0, len = this.boids.length; i < len; i++) {
                   let boid = this.boids[i];
@@ -241,15 +137,18 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                     boid.vy *= r;
                   }
 
+                  const boidWidth = this.boidImage.width;
+                  const boidHeight = this.boidImage.height;
+
                   if (
-                    (boid.x < 0 && boid.vx < 0) ||
-                    (boid.x + BOID_SIZE * pixelArt[0].length > this.view.width && boid.vx > 0) 
+                    (boid.x < boidWidth/2 && boid.vx < 0) ||
+                    (boid.x + boidWidth/2 > this.view.width && boid.vx > 0)
                   ) {
                     boid.vx *= -1;
                   }
                   if (
-                    (boid.y < 0 && boid.vy < 0) ||
-                    (boid.y + BOID_SIZE * pixelArt.length > this.view.height && boid.vy > 0)
+                    (boid.y < boidHeight/2 && boid.vy < 0) ||
+                    (boid.y + boidHeight/2 > this.view.height && boid.vy > 0)
                   ) {
                     boid.vy *= -1;
                   }
@@ -258,6 +157,7 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                   boid.y += boid.vy;
                 }
               }
+
               cohesion(index) {
                 let center = { x: 0, y: 0 };
                 let boidLength = this.boids.length;
@@ -275,6 +175,7 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                 this.boids[index].vx += (center.x - this.boids[index].x) / 100;
                 this.boids[index].vy += (center.y - this.boids[index].y) / 100;
               }
+
               separation(index) {
                 for (let i = 0, len = this.boids.length; i < len; i++) {
                   if (i === index) {
@@ -288,6 +189,7 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                   }
                 }
               }
+
               alignment(index) {
                 let average = { vx: 0, vy: 0 };
                 let boidLength = this.boids.length;
@@ -305,6 +207,7 @@ export class WebViewProvider implements vscode.WebviewViewProvider {
                 this.boids[index].vx += (average.vx - this.boids[index].vx) / 8;
                 this.boids[index].vy += (average.vy - this.boids[index].vy) / 8;
               }
+
               getDistance(boid1, boid2) {
                 let x = boid1.x - boid2.x;
                 let y = boid1.y - boid2.y;
